d8680deff132059125113e0bab488ebd76c22209
Gauree@1999 <barhegauree12@gmail.com>
2021-11-20 13:58:27 +0530

    canteen service

---
+++ Canteen Service Project RLL 199 Java PP/Angular/src/app/customer-add/customer-add.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Customer } from '../customer';
+import { HomeServiceService } from '../home-service.service';
+
+@Component({
+  selector: 'app-customer-add',
+  templateUrl: './customer-add.component.html',
+  styleUrls: ['./customer-add.component.css']
+})
+export class CustomerAddComponent implements OnInit {
+  cus:Customer;
+  msg:string;
+  insert(CustomerAdd:NgForm){
+    if(CustomerAdd.invalid){
+      return;
+ }
+    this._service.customerInsert(this.cus).subscribe(x=>{
+      this.msg=x;
+    });
+    alert(this.msg="Acount Created");
+    this._router.navigate(['/customer']);
+
+  }
+
+  constructor(private _service:HomeServiceService,private _router:Router) {
+    this.cus=new Customer();
+   }
+
+  ngOnInit(): void {
+  }
+}
